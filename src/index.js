7a41e63242432e311591f65d8d58509926a9f1f4
Victor Santos <Victor.Santos@ipsoft.com>
2019-04-02 19:19:18 -0400

    Styled component implemented and theme

--- src/index.js
+++ src/index.js
@@ -6,16 +6,24 @@ import Store from "./store";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Navigation from "components/navigation";
 import NotFound from "components/notFound";
+import { ThemeProvider } from "styled-components";
+import theme from "./common/theme/theme";
+import { MainWrapper } from "./common/styles/common.styled.js";
+import "./common/theme/fontFace.scss";
 
 ReactDOM.render(
   <Provider store={Store}>
-    <BrowserRouter>
-      <Navigation />
-      <Switch>
-        <Route path="/" component={Main} exact />
-        <Route component={NotFound} />
-      </Switch>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <MainWrapper>
+          <Navigation />
+          <Switch>
+            <Route path="/" component={Main} exact />
+            <Route component={NotFound} />
+          </Switch>
+        </MainWrapper>
+      </BrowserRouter>
+    </ThemeProvider>
   </Provider>,
   document.getElementById("mainApp")
 );
