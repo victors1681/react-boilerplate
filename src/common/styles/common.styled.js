29054388c1a5aca8c703525fb23c94f21b0e45a8
Victor Santos <Victor.Santos@ipsoft.com>
2019-04-02 19:20:54 -0400

    Adding custom font

---
+++ src/common/styles/common.styled.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const MainWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  font-family: ${({ theme }) => theme.font.family};
+  font-size: ${({ theme }) => theme.font.size};
+  color: ${({ theme }) => theme.font.color};
+  background: #f2f2f2;
+`;
---
+++ src/components/Landing/Landing.js
@@ -0,0 +1,3 @@
+import React from "react";
+import { MainWrapper } from "common/styles/common.styled.js";
+const Landing = () => <mainWrapper>s</mainWrapper>;
